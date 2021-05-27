aab57b3d8c867f6b7b0e919994f198125c02f149
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2021-05-27 18:39:41 -0300

    Atualizações e SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,13 +9,15 @@ import Document, {
 class MyDocument extends Document<DocumentProps> {
 	render(): JSX.Element {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
-					<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+					<meta charSet="UTF-8" />
+					<meta name="description" content="Template for React, with Next.js, SWR, Axios, TypeScript, ESLint, Prettier, Framer Motion, Tailwind and Sass" />
+					<link rel="shortcut icon" href="favicon.ico" type ="image/x-icon" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
-						rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
+					rel="stylesheet"
 					/>
 				</Head>
 				<body>
