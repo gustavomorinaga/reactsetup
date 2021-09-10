0150aae032a1027fe9587ab9aec2de3509a39fc3
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2021-09-09 22:05:41 -0300

    Adicionando Husky e Lint-Staged

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,23 @@
-import Document, {
-	DocumentProps,
-	Html,
-	Head,
-	Main,
-	NextScript,
-} from 'next/document';
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { DocumentProps, Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document<DocumentProps> {
+	static async getInitialProps(ctx: any) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
 	render(): JSX.Element {
 		return (
 			<Html lang="en">
 				<Head>
 					<meta charSet="UTF-8" />
 					<meta name="description" content="Template for React, with Next.js, SWR, Axios, TypeScript, ESLint, Prettier, Framer Motion, Tailwind and Sass" />
-					<link rel="shortcut icon" href="favicon.ico" type ="image/x-icon" />
+					<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
-					href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
-					rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
+						rel="stylesheet"
 					/>
 				</Head>
 				<body>
